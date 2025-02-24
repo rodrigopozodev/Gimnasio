52e26b6345b2fb51b2eb82afa0b27925a0be526e
rodrigopozodev <rodrigopozosanchez@gmail.com>
2025-02-25 00:34:54 +0100

    Crear componente para formulario de ejercicios

---
+++ frontend/src/app/modules/ejercicios/pages/formulario-ejercicio/formulario-ejercicio.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-formulario-ejercicio',
+  standalone: true,
+  imports: [],
+  templateUrl: './formulario-ejercicio.component.html',
+  styleUrl: './formulario-ejercicio.component.css'
+})
+export class FormularioEjercicioComponent {
+
+}
