eecc55ba65a77a5b7078516f610cdec95d089946
rodrigopozodev <rodrigopozosanchez@gmail.com>
2025-02-25 00:21:52 +0100

    Actualizar rutas para usar app.routes.ts en lugar de app-routing.module.ts

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'ejercicios', loadChildren: () => import('./modules/ejercicios/ejercicios.module').then(m => m.EjerciciosModule) },
+  { path: '**', redirectTo: 'ejercicios' }
+];
--- frontend/src/app/modules/ejercicios/ejercicios-routing.module.ts
+++ frontend/src/app/modules/ejercicios/ejercicios-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ListaEjerciciosComponent } from './pages/lista-ejercicios/lista-ejercicios.component';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: '', component: ListaEjerciciosComponent }];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
-export class EjerciciosRoutingModule { }
+export class EjerciciosRoutingModule {}
