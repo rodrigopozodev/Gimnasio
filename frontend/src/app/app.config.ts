a5eaf8d8db9b20caebe3fa35c731191f02575de6
rodrigopozodev <rodrigopozosanchez@gmail.com>
2025-02-25 00:34:36 +0100

    Configurar rutas y probar la lista de ejercicios

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,9 +1,14 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideClientHydration } from '@angular/platform-browser';
+import { provideHttpClient, withFetch } from '@angular/common/http';
 
 import { routes } from './app.routes';
-import { provideClientHydration } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration()]
+  providers: [
+    provideRouter(routes),
+    provideClientHydration(),
+    provideHttpClient(withFetch()) // Habilita el uso de fetch
+  ]
 };
--- frontend/src/app/modules/ejercicios/pages/lista-ejercicios/lista-ejercicios.component.ts
+++ frontend/src/app/modules/ejercicios/pages/lista-ejercicios/lista-ejercicios.component.ts
@@ -1,9 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { EjercicioService } from '../../../../core/services/ejercicio.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-lista-ejercicios',
-  templateUrl: './lista-ejercicios.component.html'
+  standalone: true,
+  imports: [CommonModule], // Agregar CommonModule
+  templateUrl: './lista-ejercicios.component.html',
 })
 export class ListaEjerciciosComponent implements OnInit {
   ejercicios: any[] = [];
