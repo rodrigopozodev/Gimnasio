d550af670cfc043b1f4d6fbd6e72e8b78cde4c65
rodrigopozodev <rodrigopozosanchez@gmail.com>
2025-02-25 00:37:02 +0100

    Implementar formulario para agregar ejercicios

--- frontend/src/app/modules/ejercicios/pages/formulario-ejercicio/formulario-ejercicio.component.ts
+++ frontend/src/app/modules/ejercicios/pages/formulario-ejercicio/formulario-ejercicio.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
+import { EjercicioService } from '../../../../core/services/ejercicio.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-formulario-ejercicio',
-  standalone: true,
-  imports: [],
   templateUrl: './formulario-ejercicio.component.html',
-  styleUrl: './formulario-ejercicio.component.css'
+  standalone: true,
+  imports: [FormsModule]
 })
 export class FormularioEjercicioComponent {
+  ejercicio = { nombre: '', musculo: '', series: 0, repeticiones: 0, peso: 0, nivel: 'principiante' };
 
+  constructor(private ejercicioService: EjercicioService) {}
+
+  agregarEjercicio() {
+    this.ejercicioService.agregarEjercicio(this.ejercicio).subscribe(() => {
+      alert('Ejercicio agregado con éxito');
+      this.ejercicio = { nombre: '', musculo: '', series: 0, repeticiones: 0, peso: 0, nivel: 'principiante' };
+    });
+  }
 }
