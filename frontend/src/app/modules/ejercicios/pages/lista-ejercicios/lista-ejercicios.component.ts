27ea47f5209a7a915b47ac69cc3c8b026b679c04
rodrigopozodev <rodrigopozosanchez@gmail.com>
2025-02-25 00:18:20 +0100

    Mostrar lista de ejercicios en Angular

--- frontend/src/app/modules/ejercicios/pages/lista-ejercicios/lista-ejercicios.component.ts
+++ frontend/src/app/modules/ejercicios/pages/lista-ejercicios/lista-ejercicios.component.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { EjercicioService } from '../../../../core/services/ejercicio.service';
 
 @Component({
   selector: 'app-lista-ejercicios',
-  standalone: true,
-  imports: [],
-  templateUrl: './lista-ejercicios.component.html',
-  styleUrl: './lista-ejercicios.component.css'
+  templateUrl: './lista-ejercicios.component.html'
 })
-export class ListaEjerciciosComponent {
+export class ListaEjerciciosComponent implements OnInit {
+  ejercicios: any[] = [];
 
+  constructor(private ejercicioService: EjercicioService) {}
+
+  ngOnInit(): void {
+    this.ejercicioService.obtenerEjercicios().subscribe(data => {
+      this.ejercicios = data;
+    });
+  }
 }
