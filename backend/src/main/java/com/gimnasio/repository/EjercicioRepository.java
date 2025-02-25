465f7d181fb71a8b54759722ec16b4d61de7b01b
rodrigopozodev <rodrigopozosanchez@gmail.com>
2025-02-25 14:20:18 +0100

    Estructura de carpetas y archivos creada

--- backend/src/main/java/com/gimnasio/repository/EjercicioRepository.java
+++
@@ -1,9 +0,0 @@
-package com.gimnasio.repository;
-
-import com.gimnasio.model.Ejercicio;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EjercicioRepository extends JpaRepository<Ejercicio, Long> {
-}
